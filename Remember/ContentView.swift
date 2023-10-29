70afa60012ffd323ca73f25f212577bf7b4ea732
redwan528 <redwankhan144@gmail.com>
2023-10-29 00:02:29 -0400

    Initial Commit

---
+++ Remember/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Remember
+//
+//  Created by Redwan Khan on 29/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Remember/RememberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberApp.swift
+//  Remember
+//
+//  Created by Redwan Khan on 29/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct RememberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
