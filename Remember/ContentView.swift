ac74c64a15751d87c7c77a4780c55785111465f2
redwan528 <redwankhan144@gmail.com>
2023-10-31 05:01:19 -0400

    Initial commit CardView isFaceUp

--- Remember/ContentView.swift
+++ Remember/ContentView.swift
@@ -5,19 +5,62 @@
 //  Created by Redwan Khan on 29/10/2023.
 //
 
-import SwiftUI
+import SwiftUI //we dont always import swiftui like when working on backend,
 
-struct ContentView: View {
+//structs are the heart of swiftUI, they can have vars and funcs, NOT A CLASS THO so no inhereitance and stuff.
+struct ContentView: View { //:View means like it behaves like a view.
+    
+//    var i: Int // but : Int is of type
+//    var s: String
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            
+            
         }
+        .foregroundColor(.orange)
+        .padding()
+
+    }
+}
+
+struct CardView: View {
+    var isFaceUp = false
+    
+    var body: some View {
+        ZStack (content: {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+            
+        })
         .padding()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
