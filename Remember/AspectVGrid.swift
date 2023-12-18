c2e1f2817b4551592a0ff3ac883ece43cdf2b94c
redwan528 <redwankhan144@gmail.com>
2023-12-18 00:49:41 -0500

    refactored ThemeManager code, added AspectVGrid file for dynamic card resizing aspect ratios per cards displayed

---
+++ Remember/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Remember
+//
+//  Created by Redwan Khan on 12/17/23.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
