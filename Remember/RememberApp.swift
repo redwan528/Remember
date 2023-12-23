0fc1836e4e99467f2c72c94bc90c1ba6dc01f295
redwan528 <redwankhan144@gmail.com>
2023-12-23 13:04:05 -0500

    organized folder structures

--- Remember/RememberApp.swift
+++ Remember/RememberApp.swift
@@ -7,10 +7,15 @@
 
 import SwiftUI
 
+// entry point of the SwiftUI app
 @main
 struct RememberApp: App {
+    //@StateObject = property wrapper for reference type
+    //that conforms to the ObservableObject protocol
+    //this creates an instance of EmojiMemoryGame which acts as the viewmodel
     @StateObject var game = EmojiMemoryGame()
     
+    //body of app protocol; describes content and behavior of app's ui
     var body: some Scene {
         WindowGroup {
             EmojiMemoryGameView(viewModel: game)
